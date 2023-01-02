c8c9fdf9bde2c5d2341856205823a2254425db48
Fayaz <fana.fayaz@gmail.com>
2023-01-02 07:14:23 +0530

    Added sop statements to the Addition.java

--- Addition.java
+++ Addition.java
@@ -4,12 +4,14 @@ class Addition
 	{
 		int num1,num2,res;
 		Scanner s = new Scanner(System.in);
+		System.out.println("Hello This program adds two integer numbers");
 		System.out.println("Enter num1 value");
 		num1=s.nextInt();
 		System.out.println("Enter num2 value");
 		num2=s.nextInt();
 		res=num1+num2:
 		System.out.println("Result is : "+res);
+		System.out.println("END OF THE PROGRAM");
 	}
 }
 
